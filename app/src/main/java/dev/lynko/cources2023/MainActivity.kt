15374a6b11ed7581c6be02ad10806759ecb273be
Lynko <alynko17@gmail.com>
2023-05-27 11:44:38 +0300

    -wip

---
+++ app/src/main/java/dev/lynko/cources2023/MainActivity.kt
@@ -0,0 +1,34 @@
+package dev.lynko.cources2023
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toolbar
+import com.bumptech.glide.Glide
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+
+        tv1.visibility = View.GONE
+        tv2.visibility = View.GONE
+        tv3.visibility = View.GONE
+
+        textViews.visibility = View.GONE
+
+
+         */
+
+    }
+
+}
