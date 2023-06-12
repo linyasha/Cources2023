cc5521014f298ef71e65010e659da7b67e957c8a
Lynko <alynko17@gmail.com>
2023-06-12 08:41:15 +0300

    lesson_12

--- app/src/main/java/dev/lynko/cources2023/MainActivity.kt
+++ app/src/main/java/dev/lynko/cources2023/MainActivity.kt
@@ -7,20 +7,48 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import android.widget.Toolbar
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+//import androidx.lifecycle.lifecycleScope
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+
+    var networkJob: Job? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launch {
-            
+
+        Log.d(Lesson12.TAG, "main start:")
+
+        networkJob = scope.launch {
+            Log.d(Lesson12.TAG, "coroutine start ${this.hashCode()}  ${scope.hashCode()}")
+            launch(Dispatchers.IO) {
+                updateData()
+            }
+//            withContext(Dispatchers.IO) {
+//
+//            }
+            Log.d(Lesson12.TAG, "coroutine end")
         }
+        Log.d(Lesson12.TAG, "main after launch ")
     }
 
+    suspend fun updateData() {
+        Log.d(Lesson12.TAG, "updateData start")
+        delay(1000L)
+        Log.d(Lesson12.TAG, "updateData end")
+        withContext(Dispatchers.Main) {
+            delay(1000L)
+            Log.d(TAG, "MAIN: ")
+        }
+        Log.d(TAG, "updateData: ")
+    }
+
+    val TAG = "d_Example"
 }
