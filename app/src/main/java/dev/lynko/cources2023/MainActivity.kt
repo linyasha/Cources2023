8e35b81438e4a98fd5edcb1b8ffce50506d89f09
Lynko <alynko17@gmail.com>
2023-06-17 13:22:55 +0300

    lesson_14

--- app/src/main/java/dev/lynko/cources2023/MainActivity.kt
+++ app/src/main/java/dev/lynko/cources2023/MainActivity.kt
@@ -1,22 +1,32 @@
 package dev.lynko.cources2023
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toolbar
-import kotlin.random.Random
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import dev.lynko.cources2023.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val database = SQLiteOpenHelper(this)
+
+        @Suppress("UNUSED_VARIABLE")
+        val arrayAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            database.getListOfTopics()
+        )
+        val cursorAdapter = CursorAdapterExample(this@MainActivity, database.getCursorWithTopics())
+
+        binding.listView.apply {
+            adapter = cursorAdapter
+        }
     }
-
 }
---
+++ app/src/main/java/dev/lynko/cources2023/SQLiteOpenHelper.kt
@@ -0,0 +1,57 @@
+package dev.lynko.cources2023
+
+import android.content.Context
+import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+internal const val TOPIC_COLUMN = "TOPIC_NAME"
+
+private const val LOG_TAG = "SQLiteOpenHelper"
+private const val DATABASE_NAME = "IT_ACADEMY_ANDROID"
+private const val TABLE_NAME = "android_table"
+private const val DATABASE_VERSION = 1
+private const val CREATE_TABLE_SQL =
+    "CREATE TABLE IF NOT EXISTS $TABLE_NAME (_id INTEGER PRIMARY KEY AUTOINCREMENT, $TOPIC_COLUMN VARCHAR(50));"
+
+class SQLiteOpenHelper(context: Context) : SQLiteOpenHelper(
+    context,
+    DATABASE_NAME,
+    null,
+    DATABASE_VERSION
+) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        try {
+            db.execSQL(CREATE_TABLE_SQL)
+            (1..5).forEach {
+                db.execSQL("INSERT INTO $TABLE_NAME ($TOPIC_COLUMN) VALUES ('Storage $it');")
+            }
+        } catch (exception: SQLException) {
+            Log.e(LOG_TAG, "Exception while trying to create database", exception)
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        Log.d(LOG_TAG, "onUpgrade called")
+    }
+
+    fun getCursorWithTopics(): Cursor {
+        return readableDatabase.rawQuery("SELECT * FROM $TABLE_NAME", null)
+    }
+
+    fun getListOfTopics(): List<String> {
+        val listOfTopics = mutableListOf<String>()
+        val cursor = getCursorWithTopics()
+        if (cursor.moveToFirst()) {
+            do {
+                val topicName = cursor.getString(cursor.getColumnIndex(TOPIC_COLUMN) ?: 0)
+                listOfTopics.add("From list: $topicName")
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        return listOfTopics
+    }
+}
